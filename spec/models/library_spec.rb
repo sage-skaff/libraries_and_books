7b917a53edfc929ce60b7c7a2c6b83fa58d7cddc
Sage Skaff <skaffsage@gmail.com>
2022-05-17 14:12:27 -0700

    Add most_recently_created test

--- spec/models/library_spec.rb
+++ spec/models/library_spec.rb
@@ -11,4 +11,15 @@ RSpec.describe Library, type: :model do
   describe 'relationships' do
     it { should have_many :books }
   end
+
+  describe 'class methods' do
+    it 'returns libraries sorted by most recently created' do
+      library1 = Library.create!(name: 'Public Library', rating: 2.9, open: true)
+      library2 = Library.create!(name: 'Oak Library', rating: 4.7, open: true)
+      library3 = Library.create!(name: 'Willow Library', rating: 3.8, open: true)
+
+      expect(Library.most_recently_created).to eq([library3, library2, library1])
+      expect(Library.most_recently_created).to_not eq([library1, library2, library3])
+    end
+  end
 end
