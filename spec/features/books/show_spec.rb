ed98dbfa6471d45bf3650be27f6c73eed189a1b1
Sage Skaff <skaffsage@gmail.com>
2022-05-17 11:54:29 -0700

    Add books show test; Update routes

---
+++ spec/features/books/show_spec.rb
@@ -0,0 +1,16 @@
+require 'rails_helper'
+
+RSpec.describe Book, type: :feature do
+  let!(:library1) { Library.create!(name: 'Public Library', rating: 2.9, open: true) }
+  let!(:book1) { library1.books.create!(name: 'The Power of Now', pages: 236, checked_out: true) }
+  let!(:book2) { Book.create!(name: 'Foundation', pages: 296, checked_out: false, library: library1) }
+  describe 'Book Show Page' do
+    it 'displays the book attributes' do
+      visit "/books/#{book1.id}"
+      expect(page).to have_content('The Power of Now')
+      expect(page).to have_content(236)
+      expect(page).to have_content('Checked Out: true')
+      expect(page).to_not have_content('Checked Out: false')
+    end
+  end
+end
