75ae03347ed8c08ff11ad6e0bf30212ca7c93f5a
Sage Skaff <skaffsage@gmail.com>
2022-05-17 11:20:58 -0700

    Add books route

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
   get '/libraries', to: 'libraries#index'
   get '/libraries/:id', to: 'libraries#show'
+  get '/books', to: 'books#index'
 end
--- spec/features/books/index_spec.rb
+++ spec/features/books/index_spec.rb
@@ -1,8 +1,9 @@
 require 'rails_helper'
 
 RSpec.describe Book, type: :feature do
-  let!(:book1) { Book.create!(name: 'The Power of Now', pages: 236, checked_out: true) }
-  let!(:book2) { Book.create!(name: 'Foundation', pages: 296, checked_out: true) }
+  let(:library1) { Library.create!(name: 'Public Library', rating: 2.9, open: true) }
+  let(:book1) { library1.books.create!(name: 'The Power of Now', pages: 236, checked_out: true) }
+  let(:book2) { Book.create!(name: 'Foundation', pages: 296, checked_out: true, library: library1) }
   describe 'Book Index Page' do
     it 'lists books' do
       visit '/books'
